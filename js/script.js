29e7e7b42b90030b32f8ae0661c40a073edb3297
cesarq88 <cesarq.089@gmail.com>
2025-05-16 20:28:30 -0300

    empezamos con javascript

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let nombre="Juan"
+let edad="40"
+let trabaja= true
+let info={
+    marca:"Toyota",
+    modelo : "Yaris",
+    year: 2015
+}
+let  count=3; 
+while(count >=0){
+    console.log(count)
+    count=count -1;
+}
+
+
+let parrafo= document.createElement("p");
+parrafo.innerText="hola";
+parrafo.style.color="red";
+document.body.appendChild(parrafo);
+
+let titulo= document.createElement("h1");
+titulo.innerText="CLASE DE JAVASCRIPT";
+titulo.style.color="BLUE";
+document.body.appendChild(titulo); 
+
