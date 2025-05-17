6b887f7cdd1c0850c6358797e45be2879804e2a9
cesarq88 <cesarq.089@gmail.com>
2025-05-16 21:00:00 -0300

    javascript funciones basicas

--- js/script.js
+++ js/script.js
@@ -21,5 +21,40 @@ document.body.appendChild(parrafo);
 let titulo= document.createElement("h1");
 titulo.innerText="CLASE DE JAVASCRIPT";
 titulo.style.color="BLUE";
-document.body.appendChild(titulo); 
+document.body.appendChild(titulo);
+
+function suma (a, b ){
+    return a+b;
+}
+function resta(a,b){
+    return a-b;
+}
+function multiplicacion(a,b){
+    return a*b;
+}
+function division (a,b){
+return a/b;
+}
+
+let a= 3 
+let b= 2 
+let opcion=2 
+ switch (opcion ){
+    case 1 :
+        console.log(suma(a,b))
+        break;
+
+    case 2 :
+        console.log(resta(a,b ))
+        break;
+    case 3 :
+        console.log(multiplicacion(a,b))
+        break;
+    case 4 : 
+    console.log(division(a,b))
+    break; 
+
+ }
+
+
 
